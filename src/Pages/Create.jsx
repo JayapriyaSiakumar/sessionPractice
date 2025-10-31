3de3df7770a038930c13559f15bcbe4c407b2813
JayapriyaSiakumar <jayapriyaeswari05@gmail.com>
2025-10-31 18:51:06 +0530

    form added

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,9 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [productName, setProductName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleName = (e) => {
+    if (e.target.value !== "") setProductName(e.target.value);
+  };
+
+  const handlePrice = (e) => {
+    if (e.target.value !== "") setPrice(e.target.value);
+  };
+  const handleDesc = (e) => {
+    if (e.target.value !== "") setDesc(e.target.value);
+  };
+  const handleImage = (e) => {
+    if (e.target.value !== "") setImage(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Success");
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl">Create</h1>
+      <form className="max-w-sm mx-auto">
+        <div className="mb-5">
+          <label
+            htmlFor="large-input"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Product Name
+          </label>
+          <input
+            type="text"
+            value={productName}
+            onChange={handleName}
+            id="large-input"
+            className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="base-input1"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Price
+          </label>
+          <input
+            type="number"
+            value={price}
+            onChange={handlePrice}
+            id="base-input1"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="base-input"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Description
+          </label>
+          <input
+            type="text"
+            value={desc}
+            onChange={handleDesc}
+            id="base-input"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="small-input"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Image
+          </label>
+          <input
+            type="url"
+            id="small-input"
+            value={image}
+            onChange={handleImage}
+            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <button
+          type="submit"
+          onSubmit={handleSubmit}
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
